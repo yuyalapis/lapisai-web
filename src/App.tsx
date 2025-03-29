231a9e355892ed22ac5f0a84b8d267e69a4a6fe6
Yuya Sato <yuyasatowork@gmail.com>
2025-03-30 07:48:37 +0900

    Update App.tsx for company site

--- src/App.tsx
+++ src/App.tsx
@@ -145,8 +145,9 @@ function App() {
                 href="#"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                現在アップデート中!!
+                Amazon にて販売予定
               </a>
+              <br />
               <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
                 発売開始まで今しばらくおまちください <span aria-hidden="true">→</span>
               </a>
