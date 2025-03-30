c3bdc919d6a5f7c4f9e62320ec11a4a9cb3f5575
Yuya Sato <yuyasatowork@gmail.com>
2025-03-30 09:33:43 +0900

    Update App.tsx for company site

--- src/App.tsx
+++ src/App.tsx
@@ -127,7 +127,7 @@ function App() {
             <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
               Treecraft<br />
               Produced by Lapis AI<br />
-              
+              By 株式会社Rozen
             </h1>
             <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
               いい栞が、ない、<br />
