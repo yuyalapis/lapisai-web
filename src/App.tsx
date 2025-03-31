463b029682efee1cbde6a17143c09db7f6be1597
Yuya Sato <yuyasatowork@gmail.com>
2025-03-31 21:21:52 +0900

    Update App.tsx added images

--- src/App.tsx
+++ src/App.tsx
@@ -168,7 +168,7 @@ function App() {
                 <br />2025-03-31: 作っているものを木の栞から木のコンピューターにアップデートしました - さぶや
               </p>
             </div>
-            <div class="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
+            <div class="w-full place-items-center">
               <img class="object-cover object-center w-full rounded-lg shadow-xl h-96 shadow-blue-gray-900/50"
                 src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80"
                 alt="nature image"
