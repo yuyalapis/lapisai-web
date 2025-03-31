c1d93d41ff148a31c813d65134b86b692d563e34
Yuya Sato <yuyasatowork@gmail.com>
2025-03-31 22:11:40 +0900

    Update App.tsx fixing images

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img alt="" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" />
+              <img alt="" src="https://logo.svgcdn.com/d/tailwindcss-original.png" className="h-8 w-auto" />
             </a>
           </div>
           <div className="flex lg:hidden">
