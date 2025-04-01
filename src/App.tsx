025dc95115586350bee669d22bb03144310462ea
Yuya Sato <yuyasatowork@gmail.com>
2025-04-01 13:53:56 +0900

    Update App.tsx bg white

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div className="bg-indigo-50">
+    <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
           <div className="flex lg:flex-1">
