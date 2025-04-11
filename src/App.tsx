15027e85d547d34c40572cfea80a9fa4c2413378
Yuya Sato <yuyasatowork@gmail.com>
2025-04-11 09:26:00 +0900

    Update App.tsx for spacing

--- src/App.tsx
+++ src/App.tsx
@@ -104,8 +104,8 @@ function App() {
         </Dialog>
       </header>
 
-      <div className="relative isolate px-6 my-12 lg:mx-8 bg-slate-100 ">
-        <div className="mx-auto max-w-2xl py-12 sm:py-12 lg:py-18">
+      <div className="relative isolate px-6 mt-12 lg:mx-8 bg-slate-100 ">
+        <div className="mx-auto max-w-2xl pt-12 sm:py-12 lg:py-18">
           <div className="w-full place-items-center">
             <img className="object-cover object-center w-full rounded-lg" src="/img_top_cover.jpg" alt="Lapis I prototype image" />
           </div>
