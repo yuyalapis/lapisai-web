f6f37fb64159be1369a5200d055ea972900be5f1
Yuya Sato <yuyasatowork@gmail.com>
2025-04-24 08:29:36 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,7 @@ function App() {
               販売アクリルポストカード一覧
             </h3>
             <div className="max-w-sm w-full lg:max-w-full lg:flex">
-              <img class="w-full" src="/reimu001.jpg" alt="Hakurei Reimu">
+              <img class="w-full" src="/reimu001.jpg" alt="Hakurei Reimu" />
               <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
                 <div className="mb-8">
                   <p className="text-sm text-gray-600 flex items-center">
