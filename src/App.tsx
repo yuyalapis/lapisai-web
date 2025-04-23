b587027f6591b841d24b4f8b0645b8f71cddd74a
Yuya Sato <yuyasatowork@gmail.com>
2025-04-24 08:07:51 +0900

    Update App.tsx for minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -133,14 +133,14 @@ function App() {
               販売アクリルポストカード一覧
             </h3>
             <div class="max-w-sm w-full lg:max-w-full lg:flex">
-  <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/img/card-left.jpg')" title="Woman holding a mug">
+  <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/img/card-left.jpg')" title="Woman holding a mug">
   </div>
-  <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-    <div class="mb-8">
-      <p class="text-sm text-gray-600 flex items-center">
+  <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+    <div className="mb-8">
+      <p className="text-sm text-gray-600 flex items-center">
         さぶや
       </p>
-      <div class="text-gray-900 font-bold text-xl mb-2">博麗霊夢 アクリルポストカード</div>
+      <div className="text-gray-900 font-bold text-xl mb-2">博麗霊夢 アクリルポストカード</div>
       
       </div>
     </div>
