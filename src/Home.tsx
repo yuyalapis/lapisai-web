862498ab94c38ba80fae9c6440eb94d1fd34132f
Yuya Sato <yuya@lapisai.com>
2025-05-12 05:24:10 +0900

    Update Home.tsx home link

--- src/Home.tsx
+++ src/Home.tsx
@@ -21,14 +21,14 @@ function App() {
         <nav aria-label="Global" className="flex items-center p-0 lg:px-8">
           
           <div className="flex flex-1 justify-center mr-auto">
-            <a href="https://lapisai.com/" className="-m-1.5 p-1.5">
+            <a target="_blank" href="https://lapisai.com/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img alt="" src="/logo.png" className="h-8 w-auto" />
             </a>
           </div>
           
           <div className="mx-4">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a target="_blank" href="https://lapisai.com/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img alt="" src="/logo_txt@4x.png" className="h-8 w-auto" />
             </a>
