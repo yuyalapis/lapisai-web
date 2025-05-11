a569c06430679fd126c745686ab3fa33797c3fd3
Yuya Sato <yuya@lapisai.com>
2025-05-12 05:01:54 +0900

    Update Query.tsx modified navbar

--- src/Query.tsx
+++ src/Query.tsx
@@ -43,22 +43,22 @@ function Query() {
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50 bg-white">
         <nav aria-label="Global" className="flex items-center p-6 lg:px-8">
-          <div className="flex-1 flex justify-center mr-auto">
+          
+          <div className="flex lg:flex-1 justify-center mr-auto">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img alt="" src="/logo.png" className="h-8 w-auto" />
-              
             </a>
           </div>
+          
           <div className="mx-12">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              
               <img alt="" src="/logo_txt@4x.png" className="h-8 w-auto" />
             </a>
           </div>
 
-          <div className="flex-1 flex justify-center ml-auto">
+          <div className="flex lg:flex-1 justify-center ml-auto">
             <a
                 href="https://lapisai.com/query"
                 target="_blank"
