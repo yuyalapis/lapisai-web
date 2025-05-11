78f84a4c113f961a32bb1b9c13ab5bb51487b275
Yuya Sato <yuya@lapisai.com>
2025-05-12 05:08:34 +0900

    Update Query.tsx modified navbar

--- src/Query.tsx
+++ src/Query.tsx
@@ -64,7 +64,7 @@ function Query() {
                 target="_blank"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                アプリ分析ツール
+                アプリ分析
               </a>
           </div>
         </nav>
