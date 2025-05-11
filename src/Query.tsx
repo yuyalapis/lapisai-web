ab6c5626c0889975a892a8aa519413afa8a7020d
Yuya Sato <yuya@lapisai.com>
2025-05-12 08:08:10 +0900

    Update Query.tsx modified link color to sky

--- src/Query.tsx
+++ src/Query.tsx
@@ -110,7 +110,7 @@ function Query() {
                     <span className="px-3 py-1 bg-emerald-500 text-white rounded-full text-xs" >{item['app_authority_score'].toFixed(1) }</span>
                   }
                   {" "}
-                  <a target="_blank" href={item['url_to']} className="-m-1.5 p-1.5 text-blue-700">
+                  <a target="_blank" href={item['url_to']} className="-m-1.5 p-1.5 text-sky-500">
                     {item['title']}
                   </a>
                 </p>
