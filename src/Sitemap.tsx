e3440088705d4d3597e987fa62e34564304581fe
Yuya Sato <yuya@lapisai.com>
2025-05-12 11:30:29 +0900

    Update Sitemap.tsx fix sitemap instance

--- src/Sitemap.tsx
+++ src/Sitemap.tsx
@@ -7,14 +7,12 @@ const fs = require('fs')
 const hostname = 'https://lapisai.com'
 
 function Sitemap() {
-  const [url_data, set_url_data] = useState({
-    "results":[
-      { url: hostname, changefreq: 'daily', priority: 1 },
-      { url: hostname+"/query", changefreq: 'daily', priority: 0.9 }
-    ]
-  })
+  const url_data = [
+    { url: hostname, changefreq: 'daily', priority: 1 },
+    { url: hostname+"/query", changefreq: 'daily', priority: 0.9 }
+  ]
   
-  const sitemap_instance = sitemap.createSitemap({hostname, url_data["results"], })
+  const sitemap_instance = sitemap.createSitemap({hostname, url_data, })
   fs.writeFileSync('./public/sitemap.xml', sitemap_instance.toString())
   
   return (
