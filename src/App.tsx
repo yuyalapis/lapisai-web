312c4a580795e32952f7db4e6865c33b0ceb7bc6
Yuya Sato <yuya@lapisai.com>
2025-05-12 11:22:22 +0900

    Update App.tsx for Sitemap

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Home from "./Home"
 import Query from "./Query"
 import Detail from "./Detail"
+import Sitemap from "./Sitemap"
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/query" element={<Query />} />
           <Route path="/app/:appname" element={<Detail />} />
+          <Route path="/sitemap" element={<Sitemap />} />
           <Route path="*" element={<h1>404</h1>} />
         </Routes>
       </BrowserRouter>
