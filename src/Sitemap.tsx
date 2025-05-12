f795f83706420bc6076995061395077519ff4c96
Yuya Sato <yuya@lapisai.com>
2025-05-12 11:20:56 +0900

    Create Sitemap.tsx

---
+++ src/Sitemap.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './App.css'
+import { useState, useEffect } from 'react'
+
+const sitemap = require('sitemap')
+const fs = require('fs')
+const hostname = 'https://lapisai.com'
+
+function Sitemap() {
+  const [url_data, set_url_data] = useState({
+    "results":[
+      { url: hostname, changefreq: 'daily', priority: 1 },
+      { url: hostname+"/query", changefreq: 'daily', priority: 0.9 }
+    ]
+  })
+  
+  const sitemap_instance = sitemap.createSitemap({hostname, url_data["results"], })
+  fs.writeFileSync('./public/sitemap.xml', sitemap_instance.toString())
+  
+  return (
+    <div className="bg-white">
+      xml rendered
+    </div>
+  );
+}
+export default Sitemap;
