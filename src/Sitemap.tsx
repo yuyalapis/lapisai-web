c4e72544a7f89f4de64f1d9516a49d444957ce6d
Yuya Sato <yuya@lapisai.com>
2025-05-12 17:43:30 +0900

    Update Sitemap.tsx minor fix

--- src/Sitemap.tsx
+++ src/Sitemap.tsx
@@ -3,35 +3,8 @@ import './App.css'
 import { useState, useEffect } from 'react'
 
 function Sitemap() {
-  const [score_data, set_score_data] = useState({
-    "results":[
-      {
-        "app_authority_score":4.0,
-        "score":15,
-        "title": "App authority score",
-        "url_to":"https://apps.apple.com/jp/app/loading.../id1664391616US"
-      },
-      {
-        "app_authority_score":4.0,
-        "score":15,"title":"アプリ信頼性スコア",
-        "url_to":"https://apps.apple.com/jp/app/ロード中です/id1244142051?l=en-US"
-      }
-    ]
-  })
   
   useEffect(() => {
-    const url_app_authority_score = "https://query-backend-topaz.vercel.app/app_authority_score"
-    fetch(url_app_authority_score).then(
-      (response) => {
-        console.log(response)
-        return response.json()
-      }
-    ).then(
-      (json_response) => {
-        console.log(json_response)
-        set_score_data(json_response)
-      }
-    )
   }, [])
   
   return (
