eccfc25520d7bc262c372b11142c88928ec54290
Yuya Sato <yuya@lapisai.com>
2025-05-12 17:37:35 +0900

    Update Sitemap.tsx minor fix

--- src/Sitemap.tsx
+++ src/Sitemap.tsx
@@ -2,16 +2,7 @@ import React from 'react'
 import './App.css'
 import { useState, useEffect } from 'react'
 
-const hostname = 'https://lapisai.com'
-
 function Sitemap() {
-    const [url_data, set_url_data] = useState({
-    "results":[
-    { url: hostname, changefreq: 'daily', priority: 1 },
-    { url: hostname+"/query", changefreq: 'daily', priority: 0.9 }
-  ]
-  })
-
   const [score_data, set_score_data] = useState({
     "results":[
       {
@@ -45,7 +36,7 @@ function Sitemap() {
   
   return (
     <div className="bg-white">
-      {url_data?.results.map(item => (
+      {score_data?.results.map(item => (
                 <p className="mt-2 text-left text-pretty text-base font-xs text-gray-800 sm:text-lg">
                   {"<url>"+item['url_to']+"</url>"}
                 </p>
