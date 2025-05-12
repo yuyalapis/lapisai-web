92e782bf10fa6b4844bf8987bbff00af69871567
Yuya Sato <yuya@lapisai.com>
2025-05-12 11:46:02 +0900

    Update Sitemap.tsx changed to cp way

--- src/Sitemap.tsx
+++ src/Sitemap.tsx
@@ -2,21 +2,39 @@ import React from 'react'
 import './App.css'
 import { useState, useEffect } from 'react'
 
-const sitemap = require('sitemap')
 const hostname = 'https://lapisai.com'
 
 function Sitemap() {
-  const url_data = [
+    const [url_data, set_url_data] = useState({
+    "results":[
     { url: hostname, changefreq: 'daily', priority: 1 },
     { url: hostname+"/query", changefreq: 'daily', priority: 0.9 }
   ]
-  
-  const sitemap_instance = sitemap.createSitemap({hostname, url_data, })
-  const sitemap_string = sitemap_instance.toString()
+  })
+
+  useEffect(() => {
+    document.title = "sitemap"
+    const url_sitemap = "https://query-backend-topaz.vercel.app/sitemap"
+    fetch(url_sitemap).then(
+      (response) => {
+        console.log(response)
+        return response.json()
+      }
+    ).then(
+      (json_response) => {
+        console.log(json_response)
+        set_url_data(json_response)
+      }
+    )
+  }, [])
   
   return (
     <div className="bg-white">
-      {sitemap_string}
+      {url_data?.results.map(item => (
+                <p className="mt-2 text-left text-pretty text-base font-semibold text-gray-700 sm:text-lg">
+                  {"<url>"} {item['url']} {"</url>"}
+                </p>
+              ))}
     </div>
   );
 }
