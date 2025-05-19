5868c97127d071f982add9985e9c78ef2479f590
Yuya Sato <yuya@lapisai.com>
2025-05-20 08:07:42 +0900

    Update Query.tsx

--- src/Query.tsx
+++ src/Query.tsx
@@ -95,6 +95,11 @@ function Query() {
               
             </p>
 
+            <a 
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              href="https://affiliate.suruga-ya.jp/modules/af/af_jump.php?user_id=5095&amp;goods_url=https%3A%2F%2Fwww.suruga-ya.jp%2Fman%2Fkaitori%2Fkaitoritop.html" rel="nofollow">【駿河屋】最新作から昔懐かしのアイテムまでなんでも買い取ります！</a>
+
+
             <h3 className="mt-8 text-left text-pretty text-xl font-semibold text-gray-900 sm:text-lg">
               App Authority Score Realtime Ranking <br /> アプリオーソリティースコア リアルタイムランキング
             </h3>
