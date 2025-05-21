e24b8bad11c580149adb22d0610bb8cc9d436226
Yuya Sato / 佐藤優矢 <yuya@lapisai.com>
2025-05-22 06:10:46 +0900

    Update UsedCardDetail.tsx size m

--- src/UsedCardDetail.tsx
+++ src/UsedCardDetail.tsx
@@ -20,7 +20,7 @@ function UsedCardDetail() {
       "title": "loading...",
       "category": "loading...",
       "region": "card",
-      "img_src": "https://www.suruga-ya.jp/database/photo.php?shinaban=107006014001&size=ss"
+      "img_src": "https://www.suruga-ya.jp/database/photo.php?shinaban=107006014001&size=m"
     },
     "keywords": "keyword",
     "results":[
