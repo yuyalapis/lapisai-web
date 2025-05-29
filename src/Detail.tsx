1cd310e20c38b8677631f280035d5b1181b058f5
Yuya Sato / 佐藤優矢 <yuya@lapisai.com>
2025-05-30 07:33:43 +0900

    Update Detail.tsx search

--- src/Detail.tsx
+++ src/Detail.tsx
@@ -173,7 +173,7 @@ function Detail() {
             <div className="flex items-center justify-center">
     
                 <form className="max-w-md mx-auto">   
-                    <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                    <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                     <div className="relative">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
