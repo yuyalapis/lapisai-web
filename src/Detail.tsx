398b69cc2c83bd0ec7c668fe57ee5dd5928b93dd
Yuya Sato / 佐藤優矢 <yuya@lapisai.com>
2025-05-30 08:22:58 +0900

    Update Detail.tsx search

--- src/Detail.tsx
+++ src/Detail.tsx
@@ -172,7 +172,7 @@ function Detail() {
 
                 <form className="max-w-md mx-auto">   
                     <div className="relative">
-                        <input type="search" id="default-search" value={search_term} onChange={(e) => {set_search_term(e.target.value)}} className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                        <input type="search" id="default-search" value={search_term} onChange={(e) => {set_search_term(e.target.value)}} className="block w-full p-4 ps-10 text-sm text-gray-900 border border-blue-800 rounded-lg bg-gray-50 focus:ring-blue-900 focus:border-blue-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="キーワードでアプリを検索" required />
                         <button type="submit" onClick={(e) => { e.preventDefault(); window.location.href = "https://lapisai.com/search/" + search_term}} className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                     </div>
                 </form>
