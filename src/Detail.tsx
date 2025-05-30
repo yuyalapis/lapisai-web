bc418b440ab9fc037b60f54ea80dc81f0167296b
Yuya Sato / 佐藤優矢 <yuya@lapisai.com>
2025-05-30 09:09:54 +0900

    Update Detail.tsx search styling

--- src/Detail.tsx
+++ src/Detail.tsx
@@ -174,7 +174,7 @@ function Detail() {
                     <div className="relative flex items-right">
                         <input type="search" id="default-search" value={search_term} onChange={(e) => {set_search_term(e.target.value)}} className="block w-full p-4 ps-10 text-sm text-gray-900 border border-blue-800 rounded-xl bg-gray-50 focus:ring-blue-900 focus:border-blue-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="キーワードでアプリを検索" required />
                         <button type="submit" onClick={(e) => { e.preventDefault(); window.location.href = "https://lapisai.com/search/" + search_term}} className="text-white mx-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                          <svg class="w-4 h-4 text-white dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                          <svg className="w-4 h-4 text-white dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                           </svg>
                         </button>
